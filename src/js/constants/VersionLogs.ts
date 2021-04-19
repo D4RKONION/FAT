6001547d290b38c64b8c743bc8418f8ab286cc75
D4RKONION <apps@fullmeter.com>
2021-04-19 20:35:51 +0100

    3.3.0 candidate

--- src/js/constants/VersionLogs.ts
+++ src/js/constants/VersionLogs.ts
@@ -1,14 +1,15 @@
 export const APP_CURRENT_VERSION_NAME = "3.3.0";
 export const APP_CURRENT_VERSION_CODE = 30300;
-export const APP_FRAME_DATA_CODE = 28;
-export const APP_DATE_UPDATED = "April 17, 2021";
+export const APP_FRAME_DATA_CODE = 29;
+export const APP_DATE_UPDATED = "April 20, 2021";
 
 
 export const VERSION_LOGS = {
   "3.3.0": {
     "New Features": [
+      "ROSE DATA WILL COME IN A FRAME DATA FILE UPDATE",
+      "This update is preparing the app for Rose so that when the framedata team are done with her they can update it immediately. When Rose is available she will show up in the character select menu",
       "New theme: Delta Green. This dark green theme is no clone!",
-      "Ready for Rose when her data is done",
       "Filter the frame data page using the search bar! You can search using a move name or an expression like oB>4",
       "Filter the character select by name using the search bar",
       "Tablet/Desktop: improved the look of character select screen",
