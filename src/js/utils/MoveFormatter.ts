79b84f728fb19a173c68f2622bf23f3657106893
Jonathan Markman <jmarkman446@gmail.com>
2021-10-17 12:54:52 -0400

    add rule for young zeku, refine formatter - added the rule for young zeku - update logic for breaking out of format function in base class - add rule to MoveFormatter

--- src/js/utils/MoveFormatter.ts
+++ src/js/utils/MoveFormatter.ts
@@ -1,12 +1,14 @@
 import BaseFormatRule from "./format_rules/BaseFormatRule";
 import MenatSF5FormatRule from "./format_rules/MenatSF5FormatRule";
+import YoungZekuSF5FormatRule from "./format_rules/YoungZekuSF5FormatRule";
 
 export default class MoveFormatter {
     rules: BaseFormatRule[];
 
     constructor() {
         this.rules = [
-            new MenatSF5FormatRule()
+            new MenatSF5FormatRule(),
+            new YoungZekuSF5FormatRule()
         ];
     }
 
