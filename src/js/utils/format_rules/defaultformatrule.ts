168feb974a223e8742538063e23e790c6e32acc3
Jonathan Markman <jmarkman446@gmail.com>
2022-02-18 22:56:03 -0500

    Add final tweaks & rules for SF4 and Third Strike

--- src/js/utils/format_rules/defaultformatrule.ts
+++ src/js/utils/format_rules/defaultformatrule.ts
@@ -7,7 +7,7 @@ export default class DefaultFormatRule extends BaseFormatRule {
     
     protected extractInput(moveData): string[] {
         let input: string[] = [];
-        let splitMove = moveData.moveName.split(' ');
+        let splitMove = moveData.moveName.trim().split(' ');
 
         input.push(splitMove[splitMove.length - 1].toUpperCase());
 
