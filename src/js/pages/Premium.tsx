bba5f06b713d844b3a35329fbe9fd1a4abf55d1c
Menardi <Menardi@users.noreply.github.com>
2024-11-30 14:57:24 +0000

    Calculate Premium tip pricing separately

--- src/js/pages/Premium.tsx
+++ src/js/pages/Premium.tsx
@@ -7,7 +7,7 @@ import { diamondSharp } from "ionicons/icons";
 import { useLocation } from "react-router";
 import ThemeSwitcher from "../components/ThemeSwitcher";
 import { purchaseLifetimePremium, resetPremium } from "../actions";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import bookmarkImage from "../../images/premium-examples/bookmarks.png"
 import ChunkyButton from "../components/ChunkyButton";
 import { APP_CURRENT_VERSION_CODE } from "../constants/VersionLogs";
@@ -22,8 +22,33 @@ const Premium = () => {
   const [premiumModalIsOpen, setPremiumModalIsOpen]  = useState(false)
   const [tipAdded, setTipAdded]  = useState(false)
 
-  const defaultPrice = "€3.49";
-  const defaultTip = "€1.50";
+  const prices = useMemo(() => {
+    const basePricing = iapStore?.get("com.fullmeter.fat.premium_lifetime")?.pricing;
+    const withTipPrice = iapStore?.get("com.fullmeter.fat.premium_lifetime_tip")?.pricing;
+
+    if (!basePricing || !withTipPrice) return null;
+
+    const priceDiff = (withTipPrice.priceMicros - basePricing.priceMicros) / 1000000;
+    const formatter = new Intl.NumberFormat(undefined, {
+      style: 'currency',
+      currency: basePricing.currency,
+      minimumFractionDigits: 2,
+      currencyDisplay: 'narrowSymbol',
+    });
+
+    return {
+      base: basePricing.price,
+      withTip: withTipPrice.price,
+      tipAmount: formatter.format(priceDiff),
+    };
+  }, [iapStore]);
+
+  const buyPremium = () => {
+    iapStore.get(
+      !tipAdded ? "com.fullmeter.fat.premium_lifetime" : "com.fullmeter.fat.premium_lifetime_tip",
+      isPlatform('android') ? CdvPurchase.Platform.GOOGLE_PLAY : CdvPurchase.Platform.APPLE_APPSTORE
+    )?.getOffer()?.order()
+  };
 
   return(
     <IonPage id="Premium">
@@ -38,7 +63,7 @@ const Premium = () => {
           <IonTitle>Premium</IonTitle>
         </IonToolbar>
       </IonHeader>
-      
+
 
       <IonContent className="ion-padding">
         {premiumIsPurchased ?
@@ -54,13 +79,13 @@ const Premium = () => {
             <article>
               <p>Hi I'm Paul! I've been developing and releasing FAT ad-free for over 10 years now. All core features and data (and pretty much everything else!) will always be free forever!</p>
               <p>However, if you'd like to support me, Premium is a <strong>one-time purchase</strong> which gives you access to a few helpful extra features as a thank you.</p>
-              {isPlatform("ios") && 
+              {isPlatform("ios") &&
                 <IonButton expand="full" shape="round" mode="ios" onClick={() => iapStore.restorePurchases()}>Restore Premium Purchase</IonButton>
               }
             </article>
           </>
-          
-            
+
+
           }
 
           <article>
@@ -71,7 +96,7 @@ const Premium = () => {
             <p>Become a true power user with unlimited bookmarks! That means quickly checking characters and moves, even across different games.</p>
             <img style={{borderRadius: "12px"}} src={bookmarkImage} alt="Bookmark example"></img>
           </section>
-            
+
 
             <section>
               <h1>App Themes</h1>
@@ -91,7 +116,7 @@ const Premium = () => {
 
           </article>
         {/* DEBUG BUTTON */}
-        {/* {!isPlatform("capacitor") && 
+        {/* {!isPlatform("capacitor") &&
           <IonFab slot="fixed" vertical="bottom" horizontal="end">
             <IonFabButton onClick={() => premiumIsPurchased ? dispatch(resetPremium()) : dispatch(purchaseLifetimePremium())}>
               <IonIcon icon={diamondSharp}></IonIcon>
@@ -105,27 +130,29 @@ const Premium = () => {
               onClick={() => setPremiumModalIsOpen(true)}
             >Upgrade to Premium!</ChunkyButton>
         }
-        
+
 
         {/* styled globally as modals are presented at root  */}
         <IonModal className="premium-offer-modal" showBackdrop={true} isOpen={premiumModalIsOpen} initialBreakpoint={1} breakpoints={[0, 1]} onDidDismiss={() => setPremiumModalIsOpen(false)}>
           <div className="premium-offer-modal-content">
-            {!premiumIsPurchased && 
-              <IonButton expand="full" shape="round" mode="ios" onClick={() => {
-                iapStore.get(
-                  !tipAdded ? "com.fullmeter.fat.premium_lifetime" : "com.fullmeter.fat.premium_lifetime_tip",
-                  isPlatform('android') ? CdvPurchase.Platform.GOOGLE_PLAY : CdvPurchase.Platform.APPLE_APPSTORE
-                )?.getOffer()?.order()
-              }}>Lifetime Premium {iapStore?.get("com.fullmeter.fat.premium_lifetime")?.pricing.price || defaultPrice}</IonButton>
-            }
-            
-            <IonItem>
-              <IonCheckbox checked={tipAdded} onIonChange={() => setTipAdded(!tipAdded)}>Add a tip for Paul (+{defaultTip}) </IonCheckbox>
-            </IonItem>
-            <p><em>Adding a tip to your purchase is completely optional, and provides no extra features. It's simply a way to give a little extra support to FAT. Either way, thank you for considering purchasing premium!</em></p>
+            {!premiumIsPurchased ? (
+              <>
+                <IonButton expand="full" shape="round" mode="ios" onClick={buyPremium}>
+                  {`Lifetime Premium ${tipAdded ? '+ Tip' : ''} ${prices ? `(${tipAdded ? prices.withTip : prices.base})` : ''}`}
+                </IonButton>
+                <IonItem>
+                  <IonCheckbox checked={tipAdded} onIonChange={() => setTipAdded(!tipAdded)}>
+                    {`Add a tip for Paul ${prices ? `(+${prices.tipAmount})` : ''}`}
+                  </IonCheckbox>
+                </IonItem>
+                <p><em>Adding a tip to your purchase is completely optional, and provides no extra features. It's simply a way to give a little extra support to FAT. Either way, thank you for considering purchasing premium!</em></p>
+              </>
+            ) : (
+              <p>Thanks for purchasing Premium. I really appreciate your support!</p>
+            )}
           </div>
         </IonModal>
-        
+
       </IonContent>
     </IonPage>
   )
