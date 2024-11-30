ee3cb4b12f2a043202fe5497070bc61ce48fc84a
Menardi <Menardi@users.noreply.github.com>
2024-11-30 15:57:05 +0000

    Set up ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: [
+    "@typescript-eslint",
+    "import",
+    "@stylistic",
+    "react-hooks",
+  ],
+  extends: [
+    "plugin:react-hooks/recommended",
+  ],
+  env: {
+    browser: true,
+  },
+  rules: {
+    "comma-dangle": ["error", "always-multiline"],
+    "import/order": ["error", {
+      alphabetize: {
+        order: "asc",
+        caseInsensitive: true,
+      },
+      groups: [
+        ["builtin", "external"],
+      ],
+      "newlines-between": "always",
+    }],
+    "no-var": "error",
+    "prefer-const": "error",
+    "@stylistic/jsx-quotes": ["error", "prefer-double"],
+    "@stylistic/key-spacing": "error",
+    "@stylistic/keyword-spacing": "error",
+    "@stylistic/indent": ["error", 2],
+    "@stylistic/no-extra-semi": "error",
+    "@stylistic/no-multi-spaces": "error",
+    "@stylistic/no-multiple-empty-lines": ["error", { max: 1 }],
+    "@stylistic/padded-blocks": ["error", "never", {
+      allowSingleLineBlocks: true,
+    }],
+    "@stylistic/quote-props": ["error", "as-needed"],
+    "@stylistic/quotes": ["error", "double", { avoidEscape: true }],
+    "@stylistic/semi": ["error", "always"],
+  },
+  ignorePatterns: [
+    "node_modules",
+    "src/js/constants/Combos.ts",
+    "src/js/constants/framedata/*.json",
+    "src/js/pages/Calculators/FrameKillGenerator.tsx",
+  ],
+};
