45c87bb7755bbb5cb66db8359eaacdb881b47575
Paul <apps@fullmeter.com>
2024-12-02 11:39:16 +0000

    No unused vars set to warn

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,7 +40,7 @@ module.exports = {
     "@stylistic/quote-props": ["error", "as-needed"],
     "@stylistic/quotes": ["error", "double", { avoidEscape: true }],
     "@stylistic/semi": ["error", "always"],
-    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/no-unused-vars": "warn",
   },
   ignorePatterns: [
     "node_modules",
