1bf2b5b7769e62e4b53444ec6209f298d5222f78
Paul <apps@fullmeter.com>
2025-01-09 15:05:24 +0000

    Reworded premium encourager

--- src/js/components/PremiumEncouragement.tsx
+++ src/js/components/PremiumEncouragement.tsx
@@ -18,13 +18,13 @@ const PremiumEncouragementModal = () => {
       initialBreakpoint={1} breakpoints={[0, 1]}
       isOpen={modalVisibility.visible && modalVisibility.currentModal === "premiumEncouragement"}
       onDidDismiss={() => modalVisibility.visible && dispatch(setModalVisibility({ currentModal: "premiumEncouragement", visible: false }))}>
-      <h2>Thanks for using FAT!</h2>
-      <p>You can unlock extra themes, bookmarks, character select layouts and more by upgrading to premium!</p>
+      <h2>Upgrade to Premium?</h2>
+      <p>Looks like you're enjoying FAT! You can unlock extra themes, bookmarks, character select layouts and more by upgrading to premium!</p>
       <ChunkyButton
         onClick={() => {
           modalVisibility.visible && dispatch(setModalVisibility({ currentModal: "premiumEncouragement", visible: false }));
           history.push("/settings/premium");
-        }}>Consider Premium!
+        }}>See Premium Features
       </ChunkyButton>
       <button onClick={() => modalVisibility.visible && dispatch(setModalVisibility({ currentModal: "premiumEncouragement", visible: false }))}>Maybe later</button>
     </IonModal>
